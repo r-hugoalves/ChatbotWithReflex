857504c1f33f0f14aa18b2bfa3bf4636dca719fd
Hugo R Alves <66341189+r-hugoalves@users.noreply.github.com>
2025-01-02 18:28:50 -0300

    Connect with OpenAI

--- ChatbotWithReflex/states.py
+++ ChatbotWithReflex/states.py
@@ -1,4 +1,6 @@
 import reflex as rx
+import os
+from openai import AsyncOpenAI
 
 class State(rx.State):
     
@@ -6,7 +8,36 @@ class State(rx.State):
     chat_history: list[tuple[str, str]]
     
     @rx.event
-    def answer(self):
-        answer = "Imagine uma resposta bem linda aqui!"
+    async def answer(self):
+        
+        # OpenAI Settings
+        client = AsyncOpenAI(
+            api_key=os.environ["OPENAI_API_KEY"]
+        )
+
+        session = await client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": self.question}
+            ],
+            stop=None,
+            temperature=0.7,
+            stream=True,
+        )
+        
+        # Add response to the answer when the chatbot responds
+        answer = ""
         self.chat_history.append((self.question, answer))
         self.question = ""
+        yield
+        
+        async for item in session:
+            if hasattr(item.choices[0].delta, "content"):
+                if item.choices[0].delta.content is None:
+                    break
+                answer += item.choices[0].delta.content
+                self.chat_history[-1] = (
+                    self.chat_history[-1][0],
+                    answer
+                )
+                yield
