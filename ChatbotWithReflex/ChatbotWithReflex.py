9353b896e6a8408dbcb215eb2a101441abacd658
Hugo R Alves <66341189+r-hugoalves@users.noreply.github.com>
2025-01-01 22:59:49 -0300

    Styling the chat

--- ChatbotWithReflex/ChatbotWithReflex.py
+++ ChatbotWithReflex/ChatbotWithReflex.py
@@ -1,14 +1,22 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-
 from rxconfig import config
+from ChatbotWithReflex import style
 
 def qa(question: str, answer:str) -> rx.Component:
     return rx.box(
-        rx.box(question, text_align="right"),
-        rx.box(answer, text_align="left"),
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right"
+        ),
+        
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left"
+        ),
         margin_y="1em",
+        width="100%"
     )
     
 def chat() -> rx.Component:
@@ -32,14 +40,21 @@ def chat() -> rx.Component:
     
 def action_bar() -> rx.Component:
     return rx.hstack(
-        rx.input(placeholder="Ask a question"),
-        rx.button("Ask!")
+        rx.input(
+            placeholder="Ask a question",
+            style=style.input_style,
+        ),
+        
+        rx.button("Ask!", style=style.button_style),
     )
     
 def index() -> rx.Component:
-    return rx.container(
-        chat(),
-        action_bar(),
+    return rx.center(
+        rx.vstack(
+            chat(),
+            action_bar(),
+            align="center",
+        )
     )
 
 app = rx.App()
